c9d92178a881950790c147f10958ec8795c7f654
lollyyks <olgaolejnik6102934@gmail.com>
2024-04-03 18:26:08 +0700

    Added func selection sort

---
+++ Practice/Controller/SortController.cs
@@ -0,0 +1,43 @@
+﻿using Practice.Model;
+
+namespace Practice.Controller;
+
+public class SortController
+{
+    public void MainController()
+    {
+        Random random = new Random();
+        int[] array = new int[random.Next(100,1000)]; 
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = random.Next(1, 100); 
+        }
+
+        Console.WriteLine("Случайный массив:");
+        foreach (int element in array)
+        {
+            Console.Write(element + " ");
+        }
+
+        Console.WriteLine("Выберите метод сортировки:");
+        Console.WriteLine("1 - Сортировка линейным выбором");
+        
+        int choice = int.Parse(Console.ReadLine());
+        while(choice < 1 || choice > 1)
+        {
+            Console.WriteLine("Некорректный выбор, введите заново!");
+            choice = int.Parse(Console.ReadLine());
+        }
+
+        switch (choice)
+        {
+            case 1:
+                Sort.SelectionSort(array);
+                break;
+        }
+        
+        Console.WriteLine("Отсортированный массив:");
+        Sort.PrintArray(array); 
+    }
+}
--- Practice/Model/Sort.cs
+++ Practice/Model/Sort.cs
@@ -2,5 +2,60 @@ namespace Practice.Model;
 
 public class Sort
 {
-    
+    // Метод для вывода массива на консоль
+    public static void PrintArray(int[] array)
+    {
+        foreach (int element in array)
+        {
+            Console.Write(element + " ");
+        }
+        Console.WriteLine();
+    }
+
+    // Метод сортировки с использованием вспомогательного массива
+    public static int[] SelectionSort(int[] arr)
+    {
+        int[] sortedArray = new int[arr.Length]; // Создаем новый массив для отсортированных элементов
+        int index = 0;
+
+        while (arr.Length > 0)
+        {
+            int maxIndex = FindMaxIndex(arr); // Находим индекс максимального элемента в исходном массиве
+            sortedArray[index] = arr[maxIndex]; // Переносим максимальный элемент во вспомогательный массив
+            index++;
+
+            arr = RemoveElement(arr, maxIndex); // Удаляем максимальный элемент из исходного массива
+        }
+
+        return sortedArray;
+    }
+
+    // Метод для поиска индекса максимального элемента в массиве
+    static int FindMaxIndex(int[] array)
+    {
+        int maxIndex = 0;
+        for (int i = 1; i < array.Length; i++)
+        {
+            if (array[i] > array[maxIndex])
+            {
+                maxIndex = i;
+            }
+        }
+        return maxIndex;
+    }
+
+    // Метод для удаления элемента из массива по индексу
+    static int[] RemoveElement(int[] arr, int index)
+    {
+        int[] newArr = new int[arr.Length - 1];
+        for (int i = 0, j = 0; i < arr.Length; i++)
+        {
+            if (i != index)
+            {
+                newArr[j] = arr[i];
+                j++;
+            }
+        }
+        return newArr;
+    }
 }
